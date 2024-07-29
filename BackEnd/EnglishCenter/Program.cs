595ddcbc0ddcbb41c75d4e6968ed9be6a152c5b2
Ngoc Anh <ngocc.anhh3009@gmail.com>
2024-07-29 17:10:14 +0700

    add system service

--- BackEnd/EnglishCenter/Program.cs
+++ BackEnd/EnglishCenter/Program.cs
@@ -1,9 +1,20 @@
+using EnglishCenter.Extensions;
 using EnglishCenter.Extensions.Database;
 using EnglishCenter.Extensions.Identity;
 using EnglishCenter.Extensions.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "DefaulPolicy",
+                      builder =>
+                      {
+                          builder.WithOrigins("http://localhost:5173")
+                                 .AllowAnyMethod()
+                                 .AllowAnyHeader();
+                      });
+});
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,6 +29,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("DefaulPolicy");
 
 app.UseAuthorization();
 
