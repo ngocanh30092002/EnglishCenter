5ad428ba99523d545aa30515d3a076d973d3143a
Ngoc Anh <ngocc.anhh3009@gmail.com>
2024-07-29 17:53:50 +0700

    add main and manage page

--- FrontEnd/english-center/src/App.jsx
+++ FrontEnd/english-center/src/App.jsx
@@ -1,10 +1,16 @@
+import { Route, Routes} from 'react-router-dom'
 import './App.css'
 import LoginPage from './components/LoginComponent/LoginPage'
+import MainPage from './components/MainComponent/MainPage'
+import ManagerPage from './components/ManagerComponent/ManagePage'
 function App() {
-
   return (
     <>
-      <LoginPage/>
+      <Routes>
+        <Route path='/' element= {<LoginPage/>}/>
+        <Route path='/main' element = {<MainPage/>}/>
+        <Route path='/manage' element = {<ManagerPage/>}/>
+      </Routes>
     </>
   )
 }
